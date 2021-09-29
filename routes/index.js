f0110908b8efdf60d6243993bf266783799815c0
Donnaya <donnayawang@gmail.com>
2021-09-29 20:43:56 +0700

    [EDIT] Routes for building and room

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
-
+const buildingController = require('../controllers/building');
+const roomController = require('../controllers/room');
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/all-building/:dormID', async (req, res) => {
+  const dormID = req.params.dormID;
+  const allBuilding = await buildingController.getBuildingByDormID(dormID);
+  res.json(allBuilding);
+});
+
+router.get('/all-room/:buildingID', async (req, res) => {
+  const buildingID = req.params.buildingID;
+  const allRoom = await await roomController.getAllRoomByBuildingID(buildingID);
+  res.json(allRoom);
 });
 
 module.exports = router;
