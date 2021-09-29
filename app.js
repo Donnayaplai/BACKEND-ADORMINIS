abaa3e2581279b5e0c611deae2339dbbc0f054a4
Donnaya <donnayawang@gmail.com>
2021-09-29 20:07:16 +0700

    [EDIT] Server config

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var express = require('express');
+const express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const db = require('./config/dbConnection');
 var indexRouter = require('./routes/index');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -13,6 +13,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+  res.json({
+    message: 'API Running',
+  });
+});
+
 app.use('/', indexRouter);
 
-module.exports = app;
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
