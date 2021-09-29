e7ec7f51c8dcf9043fb68743c786c2fa416d7533
Donnaya <donnayawang@gmail.com>
2021-09-29 20:44:14 +0700

    [EDIT] Add cors package

--- app.js
+++ app.js
@@ -2,25 +2,26 @@ const express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const db = require('./config/dbConnection');
+const cors = require('cors');
+
 var indexRouter = require('./routes/index');
 
 const app = express();
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const router = require('./routes');
+const router = require('./routes/index');
 app.use('/api', router);
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'API Running',
-  });
-});
+// app.get('/', (req, res) => {
+//   res.json({
+//     message: 'API Running',
+//   });
+// });
 
 app.use('/', indexRouter);
 
