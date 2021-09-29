4f5a70102daa5000e7b520dc91dc118014e6a6b4
Donnaya <donnayawang@gmail.com>
2021-09-29 22:00:59 +0700

    [EDIT] Get all room

--- controllers/room.js
+++ controllers/room.js
@@ -1,11 +1,26 @@
 require('sequelize');
 const roomModel = require('../models/room');
 
-exports.getAllRoomByBuildingID = async (buildingID) => {
-  const allroom = await roomModel.findAll({
-    where: {
-      BUILDINGID: buildingID,
-    },
-  });
-  return allroom;
+// exports.getAllRoomByBuildingID = async (buildingID) => {
+//   const allroom = await roomModel.findAll({
+//     where: {
+//       BUILDINGID: buildingID,
+//     },
+//   });
+//   return allroom;
+// };
+
+exports.getAllRoomByBuildingID = (req, res) => {
+  const buildingID = req.params.buildingID;
+  roomModel
+    .findAll({ where: { BUILDINGID: buildingID } })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || 'Some error occurred while retrieving all room.',
+      });
+    });
 };
