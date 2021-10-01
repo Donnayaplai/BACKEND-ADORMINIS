6f57fb5216db63c3109406d95ca6b53b5346c8ec
Donnayaplai <donnayawang@gmail.com>
2021-10-01 09:55:01 +0700

    [ADD] Setting model

---
+++ models/setting.js
@@ -0,0 +1,40 @@
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../config/dbConnection');
+
+class SETTING extends Model {}
+
+SETTING.init(
+  {
+    SETTINGID: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    WATERPRICE: {
+      type: DataTypes.DECIMAL,
+    },
+    ELECTRICITYPRICE: {
+      type: DataTypes.DECIMAL,
+    },
+    MINWATERUNIT: {
+      type: DataTypes.DECIMAL,
+    },
+    MINWATERPRICE: {
+      type: DataTypes.DECIMAL,
+    },
+    MULTPREPAID: {
+      type: DataTypes.INTEGER,
+    },
+
+    DORMID: {
+      type: DataTypes.INTEGER,
+    },
+  },
+  {
+    freezeTableName: true,
+    timestamps: false,
+    sequelize,
+    modelName: 'SETTING',
+  }
+);
+
+module.exports = SETTING;
