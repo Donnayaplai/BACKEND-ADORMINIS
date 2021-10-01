1849f214d2b3f72d1da730c3a18d3e83efa2216e
Donnayaplai <donnayawang@gmail.com>
2021-10-01 14:17:39 +0700

    [EDIT] Path and route

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors');
 
-var indexRouter = require('./routes/index');
+// var indexRouter = require('./routes/index');
 
 const app = express();
 app.use(cors());
@@ -14,8 +14,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const router = require('./routes/index');
-app.use('/api', router);
+// const router = require('./routes/index');
+// app.use('/api', router);
+
+//Room
+const roomRouter = require('./routes/room');
+app.use('/api/room', roomRouter);
+
+//building
+const buildingRouter = require('./routes/building');
+app.use('/api/building', buildingRouter);
+
+//User
+const userRouter = require('./routes/user');
+app.use('/api/user', userRouter);
+
+//Dorm
+// const dormRouter = require('./routes/dorm');
+// app.use('/dorm', dormRouter);
 
 app.get('/', (req, res) => {
   res.json({
@@ -23,7 +39,7 @@ app.get('/', (req, res) => {
   });
 });
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const buildingController = require('../controllers/building');
-const roomController = require('../controllers/room');
+
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/all-building/:dormID', buildingController.getBuildingByDormID);
-
-router.get('/all-room/:buildingID', roomController.getAllRoomByBuildingID);
+// router.get('/', function (req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
 module.exports = router;
