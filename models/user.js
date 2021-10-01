015ffe0b9d7e0b15a37b7d4feca5274dc1191f87
Donnayaplai <donnayawang@gmail.com>
2021-10-01 09:50:55 +0700

    [ADD] User model

---
+++ models/user.js
@@ -0,0 +1,50 @@
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../config/dbConnection');
+
+class USER extends Model {}
+
+USER.init(
+  {
+    USERID: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    FNAME: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    LNAME: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    TELNO: {
+      type: DataTypes.STRING,
+    },
+    GENDER: {
+      type: DataTypes.STRING,
+    },
+    IDCARDNO: {
+      type: DataTypes.STRING,
+    },
+    EMAIL: {
+      type: DataTypes.STRING,
+    },
+    PASSWORD: {
+      type: DataTypes.STRING,
+    },
+    PERSONALCODE: {
+      type: DataTypes.STRING,
+    },
+    ROLEID: {
+      type: DataTypes.INTEGER,
+    },
+  },
+  {
+    freezeTableName: true,
+    timestamps: false,
+    sequelize,
+    modelName: 'USER',
+  }
+);
+
+module.exports = USER;
