e1af89727d81d5ad37783a0adb0a30d01ab95fdb
Donnayaplai <donnayawang@gmail.com>
2021-10-01 09:52:42 +0700

    [ADD] RoomType model

---
+++ models/roomType.js
@@ -0,0 +1,27 @@
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../config/dbConnection');
+
+class ROOM_TYPE extends Model {}
+
+ROOM_TYPE.init(
+  {
+    ROOMTYPEID: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    ROOMNAME: {
+      type: DataTypes.STRING,
+    },
+    PRICE: {
+      type: DataTypes.DECIMAL,
+    },
+  },
+  {
+    freezeTableName: true,
+    timestamps: false,
+    sequelize,
+    modelName: 'ROOM_TYPE',
+  }
+);
+
+module.exports = ROOM_TYPE;
