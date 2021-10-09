a0210463b8c6b17a0f984b16493e8ed30933a4e0
Boonny <supawich.spv@mail.kmutt.ac.th>
2021-10-10 00:24:26 +0700

    [EDIT] Resident info controller

--- controllers/residentInfo.js
+++ controllers/residentInfo.js
@@ -3,10 +3,10 @@ const db = require('../config/dbConnection');
 
 const RESIDENT_INFO = async (roomID) => {
   const residentInfo = await db.query(
-    `SELECT u.USERID , u.FNAME , u.LNAME , u.TELNO , u.GENDER , u.IDCARDNO , u.EMAIL , u.PERSONALCODE ,
-            r.RENTID , r.CHECKINDATE , r.CHECKOUTDATE , r.CONTRACTOFRENTID ,
+    `SELECT u.USERID , u.FNAME , u.LNAME , u.TELNO , u.GENDER , u.IDCARDNO , u.DATEOFBIRTH, u.ADDRESS ,
+            r.RENTID , r.CHECKINDATE , r.CHECKOUTDATE ,
             r2.ROOMID , r2.ROOMNO ,r2.FLOOR , r2.BUILDINGID , r2.ROOMTYPEID , r2.STATUS ,
-            cor.STARTDATE , cor.ENDDATE 
+            cor.CONTRACTOFRENTID , cor.STARTDATE , cor.ENDDATE
     FROM USER u JOIN RENT r 
     ON u.USERID = r.USERID
     JOIN ROOM r2
