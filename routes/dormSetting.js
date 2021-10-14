d595a1429712675ef61dfa28948925fc52f5a59a
Boonny <supawich.spv@mail.kmutt.ac.th>
2021-10-14 23:35:35 +0700

    [EDIT] Setting route

--- routes/dormSetting.js
+++ routes/dormSetting.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const { createNewDorm } = require('../controllers/dorm');
-const { getCostSettingByDormID, uocCostSetting, getBuildingsByDormID, uocBuildings, updateDormInfo } = require('../controllers/setting')
+const { getCostSettingByDormID, uocCostSetting, getBuildingsByDormID, uocBuildings, getRoomTypesByDormID, uocRoomTypes, getRoomSetingByDormID, uocRoomSeting, updateDormInfo } = require('../controllers/setting')
 
 // Create new dormitory
 router.post('/', createNewDorm);
@@ -18,5 +18,17 @@ router.get('/getBuildings/:dormID', getBuildingsByDormID);
 
 // UoC building
 router.post('/setBuildings/:dormID', uocBuildings);
+
+// Get room type list
+router.get('/getRoomTypes/:dormID', getRoomTypesByDormID);
+
+// UoC room type
+router.post('/setRoomTypes/:dormID', uocRoomTypes);
+
+// Get room setting list
+router.get('/getRooms/:dormID', getRoomSetingByDormID);
+
+// UoC room setting
+router.post('/setRooms/:dormID', uocRoomSeting);
 
 module.exports = router;
