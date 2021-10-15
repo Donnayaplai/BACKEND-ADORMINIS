d81768db949c2789c9b317ec29ea94acc64976a6
Donnaya <donnayawang@gmail.com>
2021-10-15 19:11:58 +0700

    [EDIT] Add cors option

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ const cors = require('cors');
 // var indexRouter = require('./routes/index');
 
 const app = express();
-app.use(cors());
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
