d2b9d9df643a01cae55c5480bb9d6563a9a1dbf8
Boonny <supawich.spv@mail.kmutt.ac.th>
2021-10-21 21:12:20 +0700

    [EDIT] List of cost model

--- models/listOfCost.js
+++ models/listOfCost.js
@@ -9,14 +9,32 @@ LISTOFCOST.init(
             type: DataTypes.INTEGER,
             primaryKey: true,
         },
-        COSTID: {
-            type: DataTypes.DECIMAL,
-        },
-        RENTID: {
-            type: DataTypes.INTEGER,
-        },
-        COSTSTATUS: {
+        ROOM: {
             type: DataTypes.BOOLEAN,
+        },
+        ELECTRICITY: {
+            type: DataTypes.BOOLEAN,
+        },
+        WATER: {
+            type: DataTypes.BOOLEAN,
+        },
+        MAINTENANCEFEE: {
+            type: DataTypes.BOOLEAN,
+        },
+        PARKINGFEE: {
+            type: DataTypes.BOOLEAN,
+        },
+        INTERNETFEE: {
+            type: DataTypes.BOOLEAN,
+        },
+        CLEANINGFEE: {
+            type: DataTypes.BOOLEAN,
+        },
+        OTHER: {
+            type: DataTypes.BOOLEAN,
+        },
+        ROOMID: {
+            type: DataTypes.INTEGER,
         },
     },
     {
