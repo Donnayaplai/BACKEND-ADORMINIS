a183dd924efdaeda6873b20bc74d73d8909090c3
Boonny <supawich.spv@mail.kmutt.ac.th>
2021-10-21 21:13:20 +0700

    [EDIT] Room controller and route

--- controllers/room.js
+++ controllers/room.js
@@ -1,17 +1,11 @@
 require('sequelize');
 const roomModel = require('../models/room');
+const roomTypeModel = require('../models/roomType');
+const settingModel = require('../models/setting')
+const listOfCostModel = require('../models/listOfCost');
 
-// exports.getAllRoomByBuildingID = async (buildingID) => {
-//   const allroom = await roomModel.findAll({
-//     where: {
-//       BUILDINGID: buildingID,
-//     },
-//   });
-//   return allroom;
-// };
-
-exports.getAllRoomByBuildingID = (req, res) => {
-  const buildingID = req.params.buildingID;
+const getAllRoomByBuildingID = (req, res) => {
+  const { buildingID } = req.params;
   roomModel
     .findAll({ where: { BUILDINGID: buildingID } })
     .then((data) => {
@@ -24,3 +18,102 @@ exports.getAllRoomByBuildingID = (req, res) => {
       });
     });
 };
+
+const getRoomType = async (roomID) => {
+  const room = await roomModel.findOne({
+    attributes: ['ROOMTYPEID'],
+    where: {
+      ROOMID: roomID,
+    },
+  });
+
+  const roomType = await roomTypeModel.findOne({
+    attributes: ['ROOMNAME', 'PRICE'],
+    where: {
+      ROOMTYPEID: room.dataValues.ROOMTYPEID,
+    },
+  });
+
+  return roomType.dataValues;
+}
+
+const getDormSetting = async (dormID) => {
+  const setting = await settingModel.findOne({
+    where: {
+      DORMID: dormID,
+    },
+  });
+  return setting.dataValues;
+};
+
+const getRoomInfo = async (req, res) => {
+  const { dormID, roomID } = req.params;
+
+  const cost = await listOfCostModel.findOne({
+    where: {
+      ROOMID: roomID,
+    },
+  });
+
+  let listOfCost = []
+  const { ROOMNAME: roomName, PRICE: roomPrice } = await getRoomType(roomID)
+  const { MAINTENANCEFEE: maintenancePrice, PARKINGFEE: parkingPrice, INTERNETFEE: internetPrice, CLEANINGFEE: cleaningPrice, OTHER: otherPrice } = await getDormSetting(dormID);
+
+  if (cost.dataValues.MAINTENANCEFEE == true) {
+    listOfCost.push({ costId: "4", costName: "ส่วนกลาง", constPrice: maintenancePrice })
+  };
+  if (cost.dataValues.PARKINGFEE == true) {
+    listOfCost.push({ costId: "5", costName: "ที่จอดรถ", constPrice: parkingPrice })
+  };
+  if (cost.dataValues.INTERNETFEE == true) {
+    listOfCost.push({ costId: "6", costName: "อินเทอร์เน็ต", constPrice: internetPrice })
+  };
+  if (cost.dataValues.CLEANINGFEE == true) {
+    listOfCost.push({ costId: "7", costName: "รักษาความสะอาด", constPrice: cleaningPrice })
+  };
+  if (cost.dataValues.OTHER == true) {
+    listOfCost.push({ costId: "8", costName: "อื่น ๆ", constPrice: otherPrice })
+  };
+
+  return res.status(200).send({ roomName: roomName, roomPrice: roomPrice, listOfCost })
+};
+
+const removeCost = async (req, res) => {
+  const { roomID } = req.params;
+  const { costId } = req.body;
+
+  if (costId == 4) {
+    await listOfCostModel.update({ MAINTENANCEFEE: 0 }, {
+      where: {
+        ROOMID: roomID,
+      },
+    });
+  } else if (costId == 5) {
+    await listOfCostModel.update({ PARKINGFEE: 0 }, {
+      where: {
+        ROOMID: roomID,
+      },
+    });
+  } else if (costId == 6) {
+    await listOfCostModel.update({ INTERNETFEE: 0 }, {
+      where: {
+        ROOMID: roomID,
+      },
+    });
+  } else if (costId == 7) {
+    await listOfCostModel.update({ CLEANINGFEE: 0 }, {
+      where: {
+        ROOMID: roomID,
+      },
+    });
+  } else if (costId == 8) {
+    await listOfCostModel.update({ OTHER: 0 }, {
+      where: {
+        ROOMID: roomID,
+      },
+    });
+  }
+  return res.status(200).send("Success")
+};
+
+module.exports = { getAllRoomByBuildingID, getRoomInfo, removeCost };
--- routes/room.js
+++ routes/room.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const rentController = require('../controllers/rent');
-const roomController = require('../controllers/room');
+const { checkRoomStatus, addUserToRoom, editCoRAndRentInfo, removeUser } = require('../controllers/rent');
+const { getAllRoomByBuildingID, getRoomInfo, removeCost } = require('../controllers/room');
 
 // Get all room by buildingID
-router.get('/all/:buildingID', roomController.getAllRoomByBuildingID);
+router.get('/all/:buildingID', getAllRoomByBuildingID);
+
+// Check room status before add resident
+router.get('/status/:roomID', checkRoomStatus);
+
+// Room information
+router.get('/info/:dormID/:roomID', getRoomInfo);
+
+// Remove cost in specific room
+router.post('/removeCost/:roomID', removeCost);
 
 // Add resident to specific room
 router.post('/:buildingID/:roomID', async (req, res) => {
   req.params.roomID;
   req.params.buildingID;
   console.log("Add resident to room ID ", req.params.roomID);
-  await rentController.addUserToRoom(req, res);
+  await addUserToRoom(req, res);
 });
 
 // Edit Cor and rent info
@@ -19,7 +28,7 @@ router.post('/edit/:rentID/:CoRID', async (req, res) => {
   req.params.rentID;
   req.params.CoRID;
   console.log("Edit information to rent ID ", req.params.rentID);
-  await rentController.editCoRAndRentInfo(req, res);
+  await editCoRAndRentInfo(req, res);
 });
 
 // Remove resident
@@ -27,7 +36,7 @@ router.post('/remove/:roomID/:rentID', async (req, res) => {
   req.params.roomID;
   req.params.rentID;
   console.log("Remove resident from room ID ", req.params.roomID);
-  await rentController.removeUser(req, res);
+  await removeUser(req, res);
 });
 
 module.exports = router;
