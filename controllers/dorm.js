bf3413c7db1e80499b0989a32b2031d9a986e1b5
Boonny <supawich.spv@mail.kmutt.ac.th>
2021-10-22 21:05:48 +0700

    [EDIT] Dorm controller

--- controllers/dorm.js
+++ controllers/dorm.js
@@ -1,9 +1,9 @@
 require('sequelize');
 const dormModel = require('../models/dorm');
-
-const db = require('../config/dbConnection');
+const manageModel = require('../models/manage');
 
 const createNewDorm = async (req, res) => {
+  const { userID } = req.params;
   const {
     dormNameTH,
     dormNameENG,
@@ -28,31 +28,79 @@ const createNewDorm = async (req, res) => {
     DISTRICT: district ? district : null,
   };
 
-  const dormID = async () => {
-    const id = await db.query(
-      `SELECT MAX(DORMID) AS DORMID
-          FROM DORMITORY
-          `,
-      {
-        type: db.QueryTypes.SELECT,
-      }
-    );
-    return id[0].DORMID;
-  }
-  const nextDormID = String(Number(await dormID()) + 1);
+  let dormInsertId;
 
-  dormModel
-    .create(dormInfo)
-    .then((data) => {
-      res.status(200).send(data);
-    })
+  await dormModel.create(dormInfo)
+    .then(resultId => dormInsertId = resultId.null)
     .catch((err) => {
       res.status(500).send({
         message: err.message,
       });
     });
 
-  return res.status(200).send({ DORMID: nextDormID });
+  await manageModel.create({ USERID: userID, DORMID: dormInsertId })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message,
+      });
+    });
+
+  return res.status(200).send({ dormID: dormInsertId });
 };
 
-module.exports = { createNewDorm };
+const getDormInfo = async (req, res) => {
+  const { dormID } = req.params;
+
+  const dormInfo = await dormModel.findOne({
+    where: {
+      DORMID: dormID
+    },
+  })
+
+  return res.status(200).send(dormInfo.dataValues);
+};
+
+const updateDormInfo = async (req, res) => {
+  const { dormID } = req.params;
+  const {
+    dormNameTH,
+    dormNameENG,
+    address,
+    province,
+    street,
+    postCode,
+    telNo,
+    subdistrict,
+    district
+  } = req.body;
+
+  const dormInfo = {
+    DORMNAMETH: dormNameTH ? dormNameTH : null,
+    DORMNAMEENG: dormNameENG ? dormNameENG : null,
+    ADDRESS: address ? address : null,
+    PROVINCE: province ? province : null,
+    STREET: street ? street : null,
+    POSTCODE: postCode ? postCode : null,
+    TELNO: telNo ? telNo : null,
+    SUBDISTRICT: subdistrict ? subdistrict : null,
+    DISTRICT: district ? district : null,
+  };
+
+  dormModel.update(dormInfo, {
+    where: {
+      DORMID: dormID
+    }
+  })
+    .then(data => {
+      res.status(200).send(data);
+      console.log("Info updated!!");
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message
+      });
+    });
+};
+
+module.exports = { createNewDorm, getDormInfo, updateDormInfo };
