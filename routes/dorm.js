e9d9c4e9dd3c69016431a8d6b07f8e3cb59df667
Boonny <supawich.spv@mail.kmutt.ac.th>
2021-10-22 21:05:23 +0700

    [ADD] Dorm route

---
+++ routes/dorm.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const { createNewDorm, getDormInfo, updateDormInfo } = require('../controllers/dorm');
+
+// Create new dormitory
+router.post('/:userID', createNewDorm);
+
+// Get dorm info
+router.get('/info/:dormID', getDormInfo);
+
+// Update dormitory
+router.post('/update/:dormID', updateDormInfo);
+
+module.exports = router;
