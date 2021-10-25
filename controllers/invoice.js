3665b7f11a4108653bdf2a1de802bc4bdafa6f31
Boonny <supawich.spv@mail.kmutt.ac.th>
2021-10-26 00:26:43 +0700

    [EDIT] Invoice controller and route

--- controllers/invoice.js
+++ controllers/invoice.js
@@ -3,6 +3,7 @@ const invoiceModel = require('../models/invoice');
 const invoiceDetailModel = require('../models/invoiceDetail');
 const settingModel = require('../models/setting');
 const listOfCostModel = require('../models/listOfCost');
+const unitUsedModel = require('../models/unitUsed');
 const db = require('../config/dbConnection');
 
 const getDormSetting = async (dormID) => {
@@ -37,6 +38,39 @@ const getRoomPrice = async (roomID) => {
     );
     return roomPrice[0].PRICE;
 };
+
+const getListOfCostInInvoiceDetail = async (costID, invoiceID) => {
+    const listOfCost = await db.query(
+        `SELECT c.COSTNAME , id.PRICE
+            FROM INVOICE_DETAIL id
+            JOIN COST c 
+            ON id.COSTID = c.COSTID 
+            WHERE c.COSTID = :costID
+            AND id.INVOICEID = :invoiceID`,
+        {
+            replacements: { costID: costID, invoiceID: invoiceID },
+            type: db.QueryTypes.SELECT,
+        }
+    );
+    return listOfCost[0];
+};
+
+const getUnitUsed = async (roomID, billingCycle) => {
+    const unit = await db.query(
+        `SELECT ELECTRICIRYUNIT , WATERUNIT 
+        FROM UNIT_USED
+        WHERE ROOMID = :roomID
+        AND UNITUSEDDATE LIKE :billingCycle
+        ORDER BY UNITUSEDID DESC
+        LIMIT 1
+          `,
+        {
+            replacements: { roomID: roomID, billingCycle: billingCycle + '%' },
+            type: db.QueryTypes.SELECT,
+        }
+    );
+    return unit[0];
+}
 
 const createInvoice = async (req, res) => {
     const { dormID } = req.params;
@@ -122,4 +156,87 @@ const createInvoice = async (req, res) => {
     return res.status(200).send("Success");
 };
 
-module.exports = { createInvoice };
+const getAdminInvoiceList = async (req, res) => {
+    const { dormID } = req.params;
+
+    const roomList = await db.query(
+        `SELECT i.INVOICEID AS "invoiceID" , r.ROOMNO AS "roomNo" , i.TOTALPRICE AS "totalPrice" , 
+        SUBSTRING(i.INVOICEDATE,1,7) AS "billingCycle" , SUBSTRING(i.INVOICEDATE,1,4) AS "billingYear" , 
+        SUBSTRING(i.INVOICEDATE,6,2) AS "billingMonth"
+        FROM DORMITORY d JOIN BUILDING b 
+        ON d.DORMID = b.DORMID 
+        JOIN ROOM r 
+        ON b.BUILDINGID = r.BUILDINGID
+        JOIN INVOICE i 
+        ON r.ROOMID = i.ROOMID 
+        WHERE d.DORMID = ?
+        ORDER BY 4 DESC , r.FLOOR , r.ROOMNO`,
+        {
+            replacements: [dormID],
+            type: db.QueryTypes.SELECT,
+        }
+    );
+    return res.status(200).send(roomList);
+};
+
+const getInvoiceDetail = async (req, res) => {
+    const { dormID } = req.params;
+    const { invoiceID } = req.body;
+
+    const roomInvoice = await db.query(
+        `SELECT r.ROOMID , r.ROOMNO , i.INVOICEDATE , i.TOTALPRICE 
+        FROM INVOICE i 
+        JOIN ROOM r 
+        ON i.ROOMID = r.ROOMID 
+        WHERE i.INVOICEID = ?`,
+        {
+            replacements: [invoiceID],
+            type: db.QueryTypes.SELECT,
+        }
+    );
+
+    const setting = await settingModel.findOne({
+        attributes: ['WATERPRICE', 'ELECTRICITYPRICE', 'INVOICEDATE'],
+        where: {
+            DORMID: dormID
+        }
+    });
+
+    let data = {
+        roomNo: roomInvoice[0].ROOMNO,
+        invoiceDate: String(roomInvoice[0].INVOICEDATE.slice(0, 8)) + String(setting.dataValues.INVOICEDATE),
+        totalPrice: roomInvoice[0].TOTALPRICE,
+        costs: []
+    }
+
+    for (let i = 1; i <= 8; i++) {
+
+        let costID = i;
+        let unit = '';
+        let unitPrice = '';
+
+        const cost = await getListOfCostInInvoiceDetail(costID, invoiceID);
+
+        if (cost) {
+
+            if (costID == 2) {
+                unit = (await getUnitUsed(roomInvoice[0].ROOMID, roomInvoice[0].INVOICEDATE.slice(0, 8))).ELECTRICIRYUNIT;
+                unitPrice = setting.dataValues.ELECTRICITYPRICE;
+            }
+            if (costID == 3) {
+                unit = (await getUnitUsed(roomInvoice[0].ROOMID, roomInvoice[0].INVOICEDATE.slice(0, 8))).WATERUNIT;
+                unitPrice = setting.dataValues.WATERPRICE;
+            }
+
+            data.costs.push({
+                costName: cost.COSTNAME,
+                unit: unit,
+                unitPrice: unitPrice,
+                amountPrice: cost.PRICE,
+            });
+        }
+    }
+    return res.status(200).send(data);
+};
+
+module.exports = { createInvoice, getAdminInvoiceList, getInvoiceDetail };
--- routes/invoice.js
+++ routes/invoice.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { createInvoice } = require('../controllers/invoice');
+const { createInvoice, getAdminInvoiceList, getInvoiceDetail } = require('../controllers/invoice');
 
 // Create new invoice
 router.post('/create/:dormID', createInvoice);
+
+// List of invoice in admin view
+router.get('/list/:dormID', getAdminInvoiceList);
+
+// Invoice detail
+router.get('/:dormID', getInvoiceDetail);
 
 module.exports = router;
