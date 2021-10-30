a02b7b95900f6bbb1ab0396d4dd1701d75b409a3
Boonny <supawich.spv@mail.kmutt.ac.th>
2021-10-30 23:07:14 +0700

    [EDIT] Problem query

--- queries/problem.js
+++ queries/problem.js
@@ -1,20 +1,24 @@
 // Problem controller
 const getResidentComplaintList = `
     SELECT p.PROBLEMID , p.TITLE , p.INFORMEDDATE , p.STATUS 
-    FROM RENT r 
+    FROM PROBLEM p 
     JOIN USER u 
-    ON r.USERID = u.USERID 
-    JOIN PROBLEM p 
-    ON u.USERID = p.USERID 
+    ON p.USERID = u.USERID 
+    JOIN RENT r
+    ON u.USERID = r.USERID 
     WHERE r.RENTID = ?
     ORDER BY p.INFORMEDDATE DESC , p.PROBLEMID DESC;
 `
 // Problem controller
 const getAdminComplaintList = `
-    SELECT p.PROBLEMID , p.TITLE , p.INFORMEDDATE , p.STATUS 
-    FROM DORMITORY d 
-    JOIN PROBLEM p 
-    ON d.DORMID = p.DORMID 
+    SELECT p.PROBLEMID , r.ROOMNO , p.TITLE , p.INFORMEDDATE , p.STATUS 
+    FROM PROBLEM p
+    JOIN DORMITORY d 
+    ON p.DORMID = d.DORMID 
+    JOIN BUILDING b
+    ON d.DORMID = b.DORMID
+    JOIN ROOM r
+    ON b.BUILDINGID = r.BUILDINGID
     WHERE d.DORMID = ?
     ORDER BY p.INFORMEDDATE DESC , p.PROBLEMID DESC;
 `
